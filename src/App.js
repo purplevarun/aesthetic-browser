f8c7fae5d04e69bccd2c71f722507e2a968b8c32
purplevarun <purplevarun@gmail.com>
2021-04-19 14:26:34 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Home from './Home';
 function App() {
   return (
     <div className="App">
         {/*Home Page*/}
         {/*Search Results Page*/}
+        
     </div>
   );
 }
