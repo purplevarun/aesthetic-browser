82a838a4667e1abe8e653500d57aac7cd378146b
purplevarun <purplevarun@gmail.com>
2021-04-19 14:39:02 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,23 @@ import './Home.css';
 function Home() {
     return (
         <div className='home'> 
-            <h1>this is the homepage</h1>
+            //<h1>this is the homepage</h1>
+            <div className='home__header'>
+
+                <div className='home__headerLeft'></div>                
+                {}
+                {}
+                {}
+            </div>
+            <div className='home__body'>
+            
+                <div className='home__headerRight'></div>
+                {}
+                {}
+                {}
+            </div>
         </div>
     )
 }
 
-export default Home
+export default Home;
