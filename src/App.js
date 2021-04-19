b85801070df153bf2cb4e7092d70511208a0ed92
purplevarun <purplevarun@gmail.com>
2021-04-19 16:01:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Router } from "@material-ui/icons";
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      {/*Home Page*/}
-      {/*Search Results Page*/}
-      <Home />
+      <Router>
+        {/*Home Page*/}
+        {/*Search Results Page*/}
+        <Home />
+      </Router>
     </div>
   );
 }
