2ca8aa96fe727361965163576ab069fd2f415008
purplevarun <purplevarun@gmail.com>
2021-04-20 01:33:24 +0530

    .

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-
-          <Route path="/">
-            <Home />
-          </Route>
           <Route path="/search">
             <h1>this is the search page</h1>
           </Route>
-          
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </Router>
     </div>
