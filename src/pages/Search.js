9571dbf86f7dfeb6b0c7ccb694716c8ad0b9b24f
purplevarun <purplevarun@gmail.com>
2021-04-21 01:10:30 +0530

    .

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./Search.css";
 const Search = () => {
     return (
         <div className="search">
             <div className="search__input">
                 <input/>
+                <img src="search.png" width="30px"/>
             </div>
         </div>
     )
