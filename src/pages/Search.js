0ba997e7f2b97f0a4cb45546a5fd94c73155b171
purplevarun <purplevarun@gmail.com>
2021-04-21 02:42:01 +0530

    .

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import "./Search.css";
 import Button from "@material-ui/core/Button";
 import { useHistory } from 'react-router-dom';
-const Search = ({hideButtons = false}) => {
+const Search = () => {
     const [input,setInput] = useState("");
     const history=useHistory();
     const search = e => {
@@ -17,9 +17,11 @@ const Search = ({hideButtons = false}) => {
                 <img onClick={search} src="search.png" width="50px" className="search-pic"/>
             </div>
             
+            
             <div className="search__buttons">
                 <Button type="submit" onClick={search}>Search</Button>
             </div>
+            
         </form>
     )
 }
