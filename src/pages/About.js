868afb783234cf06b5998ce5abeeadbb2cd0ef24
purplevarun <purplevarun@gmail.com>
2021-04-22 01:28:16 +0530

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./About.css";
 const About = () => {
+    const linkedin = () => {
+        window.open("https://www.linkedin.com/in/varun-kedia-11061115a/");
+    }
     return (
         <div className="main_page">  
-            
+            Made By - <div className="name" onClick={linkedin}>Varun Kedia</div>
+            <br/>
+            Email - <span style={{color:"blue"}}>purplevarun@gmail.com</span>
         </div>
     )
 };
