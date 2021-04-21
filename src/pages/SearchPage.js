bc38ebfab3f5fd3b42eb8a442db24363ee8c1d85
purplevarun <purplevarun@gmail.com>
2021-04-21 15:36:07 +0530

    .

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
 import "./SearchPage.css";
 const SearchPage = () => {
+    const [{term}, dispatch] = useStateValue();
     return (
         <div className='searchPage'>
             <div className="header">
-            
+                <h1>
+                    {term}
+                </h1>
             </div>
-            <div className="">
-            
+            <div className="results">
+
             </div>
         </div>
     )
