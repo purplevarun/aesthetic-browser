49468286965058599e484e9d042f1f0c8933a06c
purplevarun <purplevarun@gmail.com>
2021-04-21 13:45:57 +0530

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
