cde503a44527f4245c8352ec5ffc1852dbb2e179
purplevarun <purplevarun@gmail.com>
2021-04-21 15:25:33 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Search from "./pages/Search";
+import SearchPage from "./pages/SearchPage";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path="/search">
-            <h1>search page </h1>
+            <SearchPage/>
           </Route>
           <Route path="/">
             <Home/>
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,6 @@
+const SearchPage = () => {
+    return (
+        <h1>hello world</h1>
+    )
+}
+export default SearchPage;
