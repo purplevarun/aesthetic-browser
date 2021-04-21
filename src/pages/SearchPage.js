d9726c23fe484f727299a9563962758b0909f1c2
purplevarun <purplevarun@gmail.com>
2021-04-21 22:27:44 +0530

    .

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,13 +2,13 @@ import React from "react";
 import { useStateValue } from "../StateProvider";
 import "./SearchPage.css";
 import useGoogleSearch from "./useGoogleSearch";
-
+import Response from "./response";
 const SearchPage = () => {
     const [{term}, dispatch] = useStateValue();
     // live api call
-    const {data} = useGoogleSearch(term);
+    // const {data} = useGoogleSearch(term);
     // local response
-    
+    const {data} = Response;
     console.log(data);
     return (
         <div className='searchPage'>
