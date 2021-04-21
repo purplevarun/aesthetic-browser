2f1f19c944fc6fa62918dc304903a1e135a3fec8
purplevarun <purplevarun@gmail.com>
2021-04-21 15:22:14 +0530

    .

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,21 @@ import React, { useState } from 'react';
 import "./Search.css";
 import Button from "@material-ui/core/Button";
 import { useHistory } from 'react-router-dom';
+import { actionTypes } from '../reducer';
+import { useStateValue } from "../StateProvider";
 const Search = () => {
+    const [{},dispatch] = useStateValue();
     const [input,setInput] = useState("");
     const history=useHistory();
     const search = e => {
         e.preventDefault();
         // alert (input);
+
+        dispatch({
+            type : actionTypes.SET_SEARCH_TERM,
+            term : input
+        })
+
         history.push('/search');
     }
     return (
